bd0e7385d669f2952edc28830b62807ea98f63d2
Pranathi <pranuk2004@gmail.com>
2026-01-17 13:50:29 +0530

    added allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
 
   preview: {
-    allowedHosts: ["social-media-frontend-st-g29.onrender.com"],
+    allowedHosts: ["social-media-project-frontend-suretrust.onrender.com"],
     host: "0.0.0.0",
     port: Number(process.env.PORT) || 4173,
   },
